51681b6a3806857fbeead956c746b8a7cc2db4cf
sneakysnail187 <maduvivier53@gmail.com>
2022-07-13 12:04:47 -0400

    door fix

--- Assets/ReactiveTarget.cs
+++ Assets/ReactiveTarget.cs
@@ -5,7 +5,7 @@ public class ReactiveTarget : MonoBehaviour {
 
 	public bool isDead;
 	public float deathAnimLength = 1.0f;
-	public float deathAngle = 90.0f;
+	public float deathAngle = 30.0f;
 	private bool startAnim = false;
 	private float currentDeathTimer = 0.0f;
 	private float angleCovered = 0.0f;
@@ -27,7 +27,7 @@ public class ReactiveTarget : MonoBehaviour {
 				currentDeathTimer = -1;
 			}
 			angleCovered += angleToRotate;
-			transform.Translate(0,0,angleToRotate);
+			transform.Translate(0,angleToRotate,0);
 		}
 	}
 
