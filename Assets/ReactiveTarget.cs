be3134bbc4fd9a4045485f1545de9d4dcc189350
sneakysnail187 <maduvivier53@gmail.com>
2022-07-15 17:15:08 -0400

    scripting for math

--- Assets/ReactiveTarget.cs
+++ Assets/ReactiveTarget.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class ReactiveTarget : MonoBehaviour {
 
@@ -9,7 +11,14 @@ public class ReactiveTarget : MonoBehaviour {
 	private bool startAnim = false;
 	private float currentDeathTimer = 0.0f;
 	private float angleCovered = 0.0f;
-	[SerializeField] private GameObject tombstonePrefab = null;
+	public TMP_Text operandOne;
+	public TMP_Text operandTwo;
+	public TMP_Text operation;
+	public TMP_Text answer;
+	int parseOne;
+	int parseTwo;
+	int answerParse;
+
 
 	void Update(){
 		if(startAnim){
@@ -32,8 +41,41 @@ public class ReactiveTarget : MonoBehaviour {
 	}
 
 	public void ReactToHit() {
-		startAnim = true;
-		StartCoroutine(Die());
+		
+		parseOne = int.Parse(operandOne.text);
+		parseTwo = int.Parse(operandTwo.text);
+		if (!int.TryParse(answer.text, out answerParse)){
+			answerParse = 0;
+		}
+		else{
+			answerParse = int.Parse(answer.text);
+		}
+
+		if(operation.text == "x"){
+			if(parseOne * parseTwo == answerParse){
+				startAnim = true;
+				StartCoroutine(Die());
+			}
+		}
+		else if(operation.text == "/"){
+			if(parseOne / parseTwo == answerParse){
+				startAnim = true;
+				StartCoroutine(Die());
+			}
+		}
+		else if(operation.text == "-"){
+			if(parseOne - parseTwo == answerParse){
+				startAnim = true;
+				StartCoroutine(Die());
+			}
+		}
+		else if(operation.text == "+"){
+			if(parseOne + parseTwo == answerParse){
+				startAnim = true;
+				StartCoroutine(Die());
+			}
+		}
+		
 	}
 
 	private IEnumerator Die() {
