dbbbff5539b6ee732aa49625e1f1d7acc3049c05
Jerry-Mubiru <40108767+Jerry-Mubiru@users.noreply.github.com>
2022-10-01 19:54:45 -0400

    THEME SONG!!
    testing the sound on the start menu with an audio manager class. Success. We now have a theme start song

---
+++ Assets/Audiomanager1.cs
@@ -0,0 +1,43 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class Audiomanager1 : MonoBehaviour
+{
+    public Sound[] sounds;
+    //Awake called before start
+    void Awake()
+    {
+        //looping through each Sound object in the Sounds array
+        foreach (Sound s in sounds){
+            //create an audioSource component
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    //void STart plays the sound on Start
+    void Start()
+    {
+        play("Theme");
+    }
+
+    //PLAY METHOD
+    public void play(string name){
+        //stores the Sound with the audioSource to be played
+        Sound toBePlayed;
+        //finding sound with the appropriate name
+        foreach (Sound s in sounds){
+            //if sound is name
+            if(s.name == name){
+                toBePlayed = s;
+                toBePlayed.source.Play();
+                return;
+            }
+        }
+        //at this point- the sound doesn't exist so throw error and return
+
+    }
+}
