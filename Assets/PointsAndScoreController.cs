150fcb509ae6c7e6b8f5be67be809f4aaacb08bb
Nofer <sxx261@case.edu>
2023-02-23 17:42:57 -0500

    Update PointsAndScoreController.cs

--- Assets/PointsAndScoreController.cs
+++ Assets/PointsAndScoreController.cs
@@ -6,18 +6,42 @@ using TMPro;
 //class to control all the math related to scores and points.
 public class PointsAndScoreController : MonoBehaviour
 {
+    public static PointsAndScoreController Instance { get; private set; }
+
+    private TMP_Text scoreBoard;
+
     //a list of variables that store different points related to different points in the game
-    public static int enemyPoints;
+    public int enemyPoints;
+    private int doorPoints;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        scoreBoard = GameObject.Find("Canvas").transform.Find("CounterBorder").GetChild(0).GetChild(0).GetComponent<TMP_Text>();
         //enemies killed are zero at beginning and updated when the player starts defeating enemies
         enemyPoints = 0;
     }
 
     //incrementing functions
-    public static void incrementEnemyPoints(){
+    public void incrementEnemyPoints()
+    {
         enemyPoints++;
+    }
+
+    public void updateDoorPoints(int points)
+    {
+        doorPoints = Mathf.Max(doorPoints + points, 0);
+        scoreBoard.text = doorPoints.ToString("00");
+    }
+
+    public void ResetPoints()
+    {
+        doorPoints = 0;
+        scoreBoard.text = "00";
     }
 }
