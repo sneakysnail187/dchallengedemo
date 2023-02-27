003b89b3d3ab426181de03ccb67d621173a71867
Nofer <sxx261@case.edu>
2023-02-26 19:43:13 -0500

    Difficulty bug fixed

--- Assets/DiffUIController.cs
+++ Assets/DiffUIController.cs
@@ -39,28 +39,28 @@ public class DiffUIController : MonoBehaviour
             buttons[0].GetComponentInChildren<TMP_Text>().text = "";
             buttons[1].GetComponentInChildren<TMP_Text>().text = "";
             buttons[2].GetComponentInChildren<TMP_Text>().text = "";
-            diffManager.GetComponent<DiffManager>().setAdd(buttonIndex);
+            diffManager.GetComponent<DiffManager>().setAdd(buttonIndex + 1);
         }
         else if (buttonIndex == 3 || buttonIndex == 4 || buttonIndex == 5){
             //this would mean we are changing in the addition range- so clear all addition
             buttons[3].GetComponentInChildren<TMP_Text>().text = "";
             buttons[4].GetComponentInChildren<TMP_Text>().text = "";
             buttons[5].GetComponentInChildren<TMP_Text>().text = "";
-            diffManager.GetComponent<DiffManager>().setAdd(buttonIndex - 3);
+            diffManager.GetComponent<DiffManager>().setAdd(buttonIndex - 2);
         }
         else if (buttonIndex == 6 || buttonIndex == 7 || buttonIndex == 8){
             //this would mean we are changing in the addition range- so clear all addition
             buttons[6].GetComponentInChildren<TMP_Text>().text = "";
             buttons[7].GetComponentInChildren<TMP_Text>().text = "";
             buttons[8].GetComponentInChildren<TMP_Text>().text = "";
-            diffManager.GetComponent<DiffManager>().setAdd(buttonIndex - 6);
+            diffManager.GetComponent<DiffManager>().setAdd(buttonIndex - 5);
         }
         else if (buttonIndex == 9 || buttonIndex == 10 || buttonIndex == 11){
             //this would mean we are changing in the addition range- so clear all addition
             buttons[9].GetComponentInChildren<TMP_Text>().text = "";
             buttons[10].GetComponentInChildren<TMP_Text>().text = "";
             buttons[11].GetComponentInChildren<TMP_Text>().text = "";
-            diffManager.GetComponent<DiffManager>().setAdd(buttonIndex - 9);
+            diffManager.GetComponent<DiffManager>().setAdd(buttonIndex - 8);
         }
         //set the button's text to a letter "o"
         buttons[buttonIndex].GetComponentInChildren<TMP_Text>().text = "o";
