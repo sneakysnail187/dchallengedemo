644b079c497be531887eda713a2b86279fe0221d
sneakysnail187 <maduvivier53@gmail.com>
2023-04-18 14:05:14 -0400

    AI fix

--- Assets/WanderingAI.cs
+++ Assets/WanderingAI.cs
@@ -7,8 +7,6 @@ public class WanderingAI : MonoBehaviour {
 	public SceneController sc;
 	public float speed = 3.0f;
 	public float obstacleRange = 4.0f;
-	public List<Transform> points;
-	int curPoint;
 	private Animator anim;
 	private bool _alive;
 	public bool broken;
@@ -23,7 +21,8 @@ public class WanderingAI : MonoBehaviour {
 	public int edgeIter = 4;
 	public float edgeDist = 0.5f;
 	public bool left = true;
-	private Vector3 minSize = new Vector3(0.5f,0.5f,0.5f);
+	public float rotSpeed = 70f;
+	private Vector3 minSize = new Vector3(0.65f,0.65f,0.65f);
 	Vector3 playLastPos = Vector3.zero;
 	Vector3 playerPos;
 	float waitTime;
@@ -31,31 +30,23 @@ public class WanderingAI : MonoBehaviour {
 	bool inRange;
 	bool near;
 	bool patrolling;
+	private bool isWandering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
 	bool seen;
 	
 	void Start() {
 		_alive = true;
-		curPoint = 0;
 		playerPos = Vector3.zero;
 		patrolling = true;
 		seen = false;
-		inRange = false;
-
-		//patrol points to be removed in favor of random wandering
-		points.Add(GameObject.Find("ppoint1").transform);
-		points.Add(GameObject.Find("ppoint1 (1)").transform);
-		points.Add(GameObject.Find("ppoint1 (2)").transform);
-		points.Add(GameObject.Find("ppoint1 (3)").transform);
-		points.Add(GameObject.Find("ppoint1 (4)").transform);
-		points.Add(GameObject.Find("ppoint1 (5)").transform);
-		points.Add(GameObject.Find("ppoint1 (6)").transform);
-		points.Add(GameObject.Find("ppoint1 (7)").transform);
+		inRange = false;	
+		Move(speed);
 
 		anim = GetComponent<Animator>();
 		navMeshAgent = GetComponent<NavMeshAgent>();
 		navMeshAgent.isStopped = false;
-		navMeshAgent.speed = speed;
-		navMeshAgent.SetDestination(points[0].position);
 		if(!broken){
 			anim.SetBool("Patrol", true);
 		}
@@ -67,9 +58,36 @@ public class WanderingAI : MonoBehaviour {
 			if(!patrolling){
 				chase();
 			}
-			else{
-				patrol();
+			else if(!isWandering) {
+				/*if(inRange){ // if player in line of sight range
+					if(rotateTime <= 0){
+						Move(5.0f); // increase run speed and search based on last known pos of player
+						LookingForP(playLastPos);
+					}
+					else{
+						//Stop();
+						//rotateTime -= Time.deltaTime;
+					}
+				}*/
+				Move(speed);
+				StartCoroutine(patrol());
 			}
+			if (isRotatingRight == true){
+				anim.SetBool("Spotted", false);
+				anim.SetBool("Patrol", false);
+                transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+			}
+			if (isRotatingLeft == true){
+				anim.SetBool("Spotted", false);
+				anim.SetBool("Patrol", false);
+				transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+			}
+			if (isWalking == true){
+				anim.SetBool("Spotted", false);
+				anim.SetBool("Patrol", true);
+				transform.position += transform.forward * speed * Time.deltaTime;
+			}
+		
 		}
 	}
 
@@ -88,7 +106,6 @@ public class WanderingAI : MonoBehaviour {
 				Move(speed); //when out of range return to patrol behavior
 				rotateTime = initRotate;
 				waitTime = initWaitTime;
-				navMeshAgent.SetDestination(points[curPoint].position);
 			}
 			else{
 				if(Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position)>=2.5f){
@@ -99,44 +116,42 @@ public class WanderingAI : MonoBehaviour {
 		}
 	}
 
-	private void patrol(){
-		if(inRange){ // if player in line of sight range
-			if(rotateTime <= 0){
-				Move(5.0f); // increase run speed and search based on last known pos of player
-				LookingForP(playLastPos);
-			}
-			else{
-				Stop();
-				rotateTime -= Time.deltaTime;
-			}
-		}
-		else{ // if player outside of line of sight
-			near = false;
-			playLastPos = Vector3.zero;
-			navMeshAgent.SetDestination(points[curPoint].position); // return to patrol
-			if(navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance){ // when bot in range of target dest
-				if(waitTime <= 0){ // when done waiting move to next patrol point (to be removed)
-					nextP();
-					Move(speed);
-					waitTime = initWaitTime;
-				}
-				else{ // stop while waiting
-					Stop();
-					waitTime -= Time.deltaTime;
-				}
-			}
-		}
+	IEnumerator patrol(){
+		// if player outside of line of sight
+		int rotTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(1, 4);
+        int rotateLorR = Random.Range(1, 2);
+        int walkWait = Random.Range(1, 5);
+        int walkTime = Random.Range(1, 6);
+		isWandering = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+		anim.SetBool("Patrol", false);
+		anim.SetBool("Spotted", false);
+        yield return new WaitForSeconds(rotateWait);
+        if (rotateLorR == 1) {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+        if (rotateLorR == 2){
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        isWandering = false;
 	}
+	
 
 	void Stop(){ // stop mving
 		navMeshAgent.isStopped = true;
 		navMeshAgent.speed = 0;
+		isWandering = false;
 	}
 
-	public void nextP(){ //move to next patrol point (to be removed)
-		curPoint = Random.Range(0, points.Count -1);
-		navMeshAgent.SetDestination(points[curPoint].position);
-	}
 
 	void LookingForP(Vector3 player){
 		navMeshAgent.SetDestination(player);
@@ -144,7 +159,6 @@ public class WanderingAI : MonoBehaviour {
 			if(waitTime <=0){
 				inRange = false;
 				Move(speed);
-				navMeshAgent.SetDestination(points[curPoint].position);
 				waitTime = initWaitTime;
 				rotateTime = initRotate;
 			}
@@ -165,6 +179,10 @@ public class WanderingAI : MonoBehaviour {
 				if(!Physics.Raycast(transform.position, toPLayer, dTP, obstacles)){ // if there are no obstacles between bot and player set varaibles accordingly
 					inRange = true;
 					patrolling = false;
+					isWandering = false;
+					isRotatingLeft = false;
+					isRotatingRight = false;
+					isWalking = false;
 				}
 				else{ // else not in range
 					inRange = false;
@@ -217,4 +235,6 @@ public class WanderingAI : MonoBehaviour {
 		//Add to the AI death count on the canvas - This is handled by the "PointsAndScoreController" class
 		PointsAndScoreController.Instance.incrementEnemyPoints();
 	}
+
+
 }
